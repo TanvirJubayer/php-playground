6e56ef822c8fa067d372ac3298c4b27ca27c8937
Tanvir_Jobayer <tnvirjubayer96@gmail.com>
2026-01-10 13:14:05 +0600

    Php_variable_&_if/else_form

---
+++ form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="task">
+        <button>Add Task</button>
+    </form>
+</body>
+</html>
+    <?php
+        // echo $_POST['task'] ?? '';
+
+        // echo "you added:" . $_POST["task"] ?? '';
+
+        // echo "You typed: " . ($_POST['task'] ?? '');
+        
+        if (isset($_POST['task'])){
+            echo "you typed: " . $_POST['task'];
+        }
+
---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+
+//ChatGPT:Learning PHP Step-by-Step:-
+
+
+
+
+echo "hello PHP";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "This is Tanvir Jobayer";
+
+echo "<br>";
+echo "<br>";
+
+$name = "Tanvir";
+$age = 29;
+$value = true;
+echo "My Name is $name";
+echo "<br>";
+echo "I am $age years old";
+echo "<br>";
+// print_r "$value declared";
+echo "<br>";
+echo "<br>";
+$coffee = 120;
+$burger = 250;
+$sum = $coffee + $burger;
+echo "Coffee: $coffee taka";
+echo "<br>";
+echo "Burgera: $burger taka";
+echo "<br>";
+echo "Total: $sum taka";
+echo "<br>";
+
+echo "<br>";
+echo "<br>";
+$money = 2500;
+if ($money>=1990){
+    echo "I am Rich";
+} else{
+    echo "I am Broke";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $name = "Tanvir Jobayer";
+        echo "<h1>Welcome, $name...</h1>"
+    ?>
+</body>
+</html>
+
